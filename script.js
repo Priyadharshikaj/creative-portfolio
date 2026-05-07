d714932294b9f1e181972a7253c26c8ac8aea590
Priyadharshikaj <priyadharshikaj@gmail.com>
2026-05-07 16:02:22 +0530

    My portfolio website

---
+++ script.js
@@ -0,0 +1,17 @@
+const text = "Aspiring Frontend Developer";
+
+let i = 0;
+
+function typing(){
+
+  if(i < text.length){
+
+    document.getElementById("typing").innerHTML += text.charAt(i);
+
+    i++;
+
+    setTimeout(typing,100);
+  }
+}
+
+typing();
